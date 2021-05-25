99081cd97649ab8702c8570a5c3d3d188ee9cbd5
Andrea <andrea3aprile99@gmail.com>
2021-05-25 16:43:30 +0200

    s

--- diet-main/js/script.js
+++ diet-main/js/script.js
@@ -41,57 +41,54 @@ var app = new Vue({
 
         if (this.selected == 'poca') {
         this.Calorie = this.Calorie * 0.9  ;
+          this.Calorie = this.Calorie.toFixed(2);
         } else if (this.selected == 'media') {
         this.Calorie = this.Calorie ;
+          this.Calorie = this.Calorie.toFixed(2);
         }  else  {
         this.Calorie = this.Calorie * 1.1 ;
+          this.Calorie = this.Calorie.toFixed(2);
         }
 
 
         this.Bmi = this.Peso / ((this.Altezza /100) * (this.Altezza/100));
-
+         var pesodaperdere = this.PesoIdeale - this.Peso;
          var pesodaaumentare = this.Peso - this.PesoIdeale;
          var pesooo2 = (this.PesoIdeale * 2);
-
 
          var pesooo = (pesodaaumentare + pesooo2)/2;
 
 
 
-
         if (this.PesoIdeale < this.Peso) {
-            document.getElementById('pesodasistemare').innerHTML= 'perdere' +  pesodaaumentare;
+            document.getElementById('pesodasistemare').innerHTML= 'perdere' +  pesodaperdere.toFixed(2);
         }else if(this.PesoIdeale == this.Peso){
             document.getElementById('pesodasistemare').innerHTML= 'seguire una diete che continui con un apporto calorico e attivita fisica uguale a quella che stai gia attuando,ottimo';
         }else {
-          document.getElementById('pesodasistemare').innerHTML= 'aumentare  di' + (-pesodaaumentare);
+          document.getElementById('pesodasistemare').innerHTML= 'aumentare apporto calorico di' + pesodaaumentare.toFixed(2);
         }
 
-
-         var x = pesodaaumentare; x = Math.abs(x);
-
-
+        var x = pesodaaumentare; x = Math.abs(x);
         if (x < 5) {
-        document.getElementById('pesocheaumenta').innerHTML= 'Consigliamo di perdere '+ x + 'in tre mesi per non accellerare il tuo metabolismo';
-        xy=3;
-      } else if (x > 5 && x < 10) {
-        document.getElementById('pesocheaumenta').innerHTML= 'Consigliamo di perdere '+ x + 'in sei mesi per non accellerare il tuo metabolismo';
-          xy=6;
-        } else if (x > 10 && x < 20) {
-        document.getElementById('pesocheaumenta').innerHTML= 'Consigliamo di perdere '+ x + 'in 12 mesi per non accellerare il tuo metabolismo';
-          xy=12;
-        } else {
-        document.getElementById('pesocheaumenta').innerHTML= 'Consigliamo di perdere '+ x + 'in 18 mesi per non accellerare il tuo metabolismo';
-          xy=18;
-        }
-
-         // var pesorazionatoingiorni = x/xy;
-         // document.getElementById('pesorazionatoingiorni').innerHTML=pesorazionatoingiorni;
-         // var kcalingiorni = pesorazionatoingiorni * 250 ;
-         // this.Caloriemeno = this.Calorie - kcalingiorni;
-         // this.Calorieaumento = this.Calorie + kcalingiorni;
+      document.getElementById('pesocheaumenta').innerHTML= 'Consigliamo di perdere '+ x + 'in tre mesi per non accellerare il tuo metabolismo';
+      xy=3;
+    } else if (x > 5 && x < 10) {
+      document.getElementById('pesocheaumenta').innerHTML= 'Consigliamo di perdere '+ x + 'in sei mesi per non accellerare il tuo metabolismo';
+        xy=6;
+      } else if (x > 10 && x < 20) {
+      document.getElementById('pesocheaumenta').innerHTML= 'Consigliamo di perdere '+ x + 'in 12 mesi per non accellerare il tuo metabolismo';
+        xy=12;
+      } else {
+      document.getElementById('pesocheaumenta').innerHTML= 'Consigliamo di perdere '+ x + 'in 18 mesi per non accellerare il tuo metabolismo';
+        xy=18;
+      }
 
 
+
+      var mediax = xy/2;
+ var pesorazionatoingiorni = x/xy;
+ document.getElementById('pesorazionatoingiorni').innerHTML=pesorazionatoingiorni.toFixed(2);
+ var kcalingiorni = pesorazionatoingiorni * 250 ;
 
 
         var percCarb = 0.35 * this.Calorie;
@@ -102,81 +99,54 @@ var app = new Vue({
           document.getElementById('percProte').innerHTML=percProte;
 // grafico mesi
 
-        const d = new Date();
+          var currentDate = new Date();
 
-        const monthName = d.toLocaleString("default", {month: "long"});
-
-
-
+  const monthName = d.toLocaleString("default", {month: "long"});
 
 
 
 
 // canvas grafico
-          var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById('myChart').getContext('2d');
 
-          var myChart = new Chart(ctx, {
-              type: 'line',
-              data: {
-                  labels: [monthName, xy ],
-                  datasets: [{
-                      label: '# of Votes',
-                      data: [this.Peso,pesooo, this.PesoIdeale],
-                      backgroundColor: [
-                          'rgba(255, 99, 132, 0.2)',
-                          'rgba(54, 162, 235, 0.2)',
-                          'rgba(255, 206, 86, 0.2)',
-                          'rgba(75, 192, 192, 0.2)',
-                          'rgba(153, 102, 255, 0.2)',
-                          'rgba(255, 159, 64, 0.2)'
-                      ],
-                      borderColor: [
-                          'rgba(255, 99, 132, 1)',
-                          'rgba(54, 162, 235, 1)',
-                          'rgba(255, 206, 86, 1)',
-                          'rgba(75, 192, 192, 1)',
-                          'rgba(153, 102, 255, 1)',
-                          'rgba(255, 159, 64, 1)'
-                      ],
-                      borderWidth: 1
-                  }]
-              },
-              options: {
-                  scales: {
-                      y: {
-                          beginAtZero: true
-                      }
-                  }
-              }
-          });
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [monthName,mediax, xy ],
+        datasets: [{
+            label: '# of Votes',
+            data: [this.Peso,pesooo, this.PesoIdeale],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
-      //     var ctxx = document.getElementById('myChart1').getContext('2d');
-      //
-      //   var myChart = new Chart(ctxx, {
-      //     const config = {
-      //     type: 'doughnut',
-      //     data: data,
-      //   };
-      //   const data = {
-      //     labels: [
-      //       'Red',
-      //       'Blue',
-      //       'Yellow'
-      //     ],
-      //     datasets: [{
-      //       label: 'My First Dataset',
-      //       data: [300, 50, 100],
-      //       backgroundColor: [
-      //         'rgb(255, 99, 132)',
-      //         'rgb(54, 162, 235)',
-      //         'rgb(255, 205, 86)'
-      //       ],
-      //       hoverOffset: 4
-      //     }]
-      //   }
-      // })
+
       },
-    Prova1:function(){
+      Prova1:function(){
 
 
       if (this.Sesso == 'Uomo') {
@@ -188,6 +158,13 @@ var app = new Vue({
       } else {
         this.Calorie = 22.1 + (this.Peso  * 31.05 ) + (this.Altezza  * 1.16) ;
           this.PesoIdeale = ((this.Altezza /100) * (this.Altezza/100)) * 20.6;
+      }
+      if (this.selected == 'poca') {
+      this.Calorie = this.Calorie * 0.9  ;
+      } else if (this.selected == 'media') {
+      this.Calorie = this.Calorie ;
+      }  else  {
+      this.Calorie = this.Calorie * 1.1 ;
       }
 
       var percCarb = 0.35 * this.Calorie;
@@ -263,6 +240,25 @@ var app = new Vue({
       // outer grafico ciambella
 
 
+
+    /*  getRandomInt:function (max){
+        return Math.floor(Math.random() * max);
+      },
+      percentuale:function(){
+      do {
+        RProte =getRandomInt(this.percProte);
+        RGrassi =getRandomInt(this.percGrassi);
+        RCarb =getRandomInt(this.percCarb);
+        this.i++;
+      }
+      while ((RProte+RGrassi+RCarb)==this.Calorie);
+
+      if ((RProte+RGrassi+RCarb)==this.Calorie) {
+          document.getElementById(randomPRG).innerHTML='RProte'+'RGrassi'+'RCarb';
+      }
+    }*/
+    }
+  })
 
     /*  getRandomInt:function (max){
         return Math.floor(Math.random() * max);
